bcc9e9fc3b18d1fdfd069a6ac0c294bcd89d54d4
Hadley Wickham <h.wickham@gmail.com>
2026-04-03 16:14:57 -0500

    Break up in to more files

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,11 @@ local_load_all <- function(path, env = parent.frame()) {
   pkgload::load_all(path, quiet = TRUE)
   withr::defer(pkgload::unload(path), envir = env)
 }
+
+last <- function(x, n = 0) {
+  if (length(x) <= n) {
+    x[NA_integer_]
+  } else {
+    x[[length(x) - n]]
+  }
+}
